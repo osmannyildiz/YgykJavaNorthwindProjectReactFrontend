ee0685b647b118af13e1723903090e12c0ad71d3
Osman Nuri Yıldız <iamosmannyildiz@gmail.com>
2022-03-13 14:11:41 +0300

    Enable React StrictMode

--- src/index.js
+++ src/index.js
@@ -11,13 +11,14 @@ import configureStore from "./store/configureStore";
 const store = configureStore();
 
 ReactDOM.render(
-	/*<React.StrictMode>*/
-	<Provider store={store}>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</Provider>,
-	/*</React.StrictMode>*/ document.getElementById("root")
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
